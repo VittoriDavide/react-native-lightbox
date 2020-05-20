7494577d61850d4f3f900f5b36304d8098f2c953
David Vittori <me@vittoridavid.com>
2020-05-20 19:56:49 +0200

    added native driver to true

--- LightboxOverlay.js
+++ LightboxOverlay.js
@@ -98,7 +98,7 @@ export default class LightboxOverlay extends Component {
       },
       onPanResponderMove: Animated.event([
         null,
-        { dy: this.state.pan }
+        { dy: this.state.pan, }
       ]),
       onPanResponderTerminationRequest: (evt, gestureState) => true,
       onPanResponderRelease: (evt, gestureState) => {
@@ -115,7 +115,7 @@ export default class LightboxOverlay extends Component {
         } else {
           Animated.spring(
             this.state.pan,
-            { toValue: 0, ...this.props.springConfig }
+            { toValue: 0, useNativeDriver: true, ...this.props.springConfig }
           ).start(() => { this.setState({ isPanning: false }); });
         }
       },
@@ -144,7 +144,7 @@ export default class LightboxOverlay extends Component {
 
     Animated.spring(
       this.state.openVal,
-      { toValue: 1, ...this.props.springConfig }
+      { toValue: 1, useNativeDriver: true, ...this.props.springConfig }
     ).start(() => {
       this.setState({ isAnimating: false });
       this.props.didOpen();
@@ -161,7 +161,7 @@ export default class LightboxOverlay extends Component {
     });
     Animated.spring(
       this.state.openVal,
-      { toValue: 0, ...this.props.springConfig }
+      { toValue: 0, useNativeDriver: true, ...this.props.springConfig }
     ).start(() => {
       this.setState({
         isAnimating: false,
